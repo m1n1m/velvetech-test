bbd97d00906a167f100c1e1bbf6da8a8564918e8
Alex <osipenko.aleksey@gmail.com>
2021-10-07 18:15:32 +0300

    before crud

--- front/src/services/GoodsService.ts
+++ front/src/services/GoodsService.ts
@@ -1,21 +1,29 @@
 import { httpClient, HttpResponse } from '@api';
 import dayjs from 'dayjs';
 
+const cat1 = {id: '1', name: 'Алкоголь крепкий'};
+const cat17 = {id: '17', name: 'Напитки'};
+const mockGoods = [
+    {id: '1', name: 'абсент', price: 1000.11, expirationDate: dayjs().toDate(), category: cat1},
+    {id: '2', name: 'аквавит', price: 555.33, expirationDate: dayjs().toDate(), category: cat17},
+    {id: '3', name: 'арак', price: 234.44, expirationDate: dayjs().toDate(), category: cat1},
+    {id: '4', name: 'бренди', price: 4666.33, expirationDate: dayjs().toDate(), category: cat1},
+    {id: '5', name: 'виски', price: 8765.2, expirationDate: dayjs().toDate(), category: cat1},
+    {id: '6', name: 'джин', price: 235.33, expirationDate: dayjs().toDate(), category: cat1},
+    {id: '7', name: 'маотай', price: 72352.22, expirationDate: dayjs().toDate(), category: cat1},
+];
+
 export default {
     findAll(): Promise<HttpResponse> {
         return new Promise<HttpResponse>(resolve => {
-            const cat1 = {id: 1, name: 'Алкоголь крепкий'};
-            const cat17 = {id: 17, name: 'Напитки'};
-            resolve(new HttpResponse([
-                {id: 1, name: 'абсент', price: 1000.11, expirationDate: dayjs().toDate(), category: cat1},
-                {id: 2, name: 'аквавит', price: 555.33, expirationDate: dayjs().toDate(), category: cat17},
-                {id: 3, name: 'арак', price: 234.44, expirationDate: dayjs().toDate(), category: cat1},
-                {id: 4, name: 'бренди', price: 4666.33, expirationDate: dayjs().toDate(), category: cat1},
-                {id: 5, name: 'виски', price: 8765.2, expirationDate: dayjs().toDate(), category: cat1},
-                {id: 6, name: 'джин', price: 235.33, expirationDate: dayjs().toDate(), category: cat1},
-                {id: 7, name: 'маотай', price: 72352.22, expirationDate: dayjs().toDate(), category: cat1},
-            ]))
+            resolve(new HttpResponse(mockGoods));
         })
         // return httpClient.get('/api/v1/categories');
+    },
+
+    findById(id: string): Promise<HttpResponse> {
+        return new Promise<HttpResponse>(resolve => {
+            resolve(new HttpResponse(mockGoods.find(g => g.id === id)));
+        })
     },
 };
