02d81c33591f2068400444f930055d17931f6ce3
Alex <osipenko.aleksey@gmail.com>
2021-10-09 12:58:23 +0300

    react hooks

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -7,48 +7,55 @@ import {useStore} from '@store/stores';
 
 const Login = () => {
 
-    const { rootStore } = useStore();
+    const {rootStore} = useStore();
 
     function authorize() {
         rootStore.setIsAuthorized(true);
     }
 
     return (
-        <Container className={appStyles.container} maxWidth="xs">
-            <form>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField fullWidth label="Email" name="email" size="small" variant="outlined"/>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    fullWidth
-                                    label="Password"
-                                    name="password"
-                                    size="small"
-                                    type="password"
-                                    variant="outlined"
-                                />
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{minHeight: '100vh'}}
+        >
+            <Container className={appStyles.container} maxWidth="xs">
+                <form>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField fullWidth label="Email" name="email" size="small" variant="outlined"/>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        label="Password"
+                                        name="password"
+                                        size="small"
+                                        type="password"
+                                        variant="outlined"
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                color="primary"
+                                fullWidth
+                                variant="contained"
+                                onClick={authorize}
+                            >
+                                Log in
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            color="primary"
-                            fullWidth
-                            variant="contained"
-                            onClick={authorize}
-                        >
-                            Log in
-                        </Button>
-                    </Grid>
-                </Grid>
-            </form>
-        </Container>
+                </form>
+            </Container>
+        </Grid>
     );
 }
 
